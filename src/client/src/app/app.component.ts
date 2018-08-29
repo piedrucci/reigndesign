be85df9a2486541fbb9c605b4150914e10c6c5c5
Roberth Mejias <piedrucci@gmail.com>
2018-08-29 02:07:45 -0300

    article list

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  articles: Article;
+  displayedColumns: string[] = ['title', 'created_at', 'author'];
+
+  constructor(private dataProvider: AppService) {}
+
+  ngOnInit() {
+    const response = this.dataProvider.fetchData();
+    response.subscribe((data: Article) => {
+      this.articles = data;
+    });
+  }
+
+  removeArticle(id) {
+    alert(id);
+  }
 }
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatTableModule, MatIconModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DateFormatPipe } from './date-format.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateFormatPipe
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
